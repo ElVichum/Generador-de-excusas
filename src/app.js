47f11c636175a5d04cb180580a3b1efcd23b708e
ElVichum <100027914+ElVichum@users.noreply.github.com>
2024-09-01 16:55:51 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let sujeto = [
+    "Pablo motos ",
+    "Mi madre ",
+    "Mi primo chico ",
+    "Carmen Machi "
+  ];
+  let accion = [
+    "se comió ",
+    "me ha roto ",
+    "ha hecho desaparecer ",
+    "se ha encargado de ",
+    "ha encogido a "
+  ];
+  let objeto = ["mi movil ", "mi tio ", "el coche ", "los niños "];
+  let tiempo = [
+    "anoche",
+    "el enero del año pasado",
+    "esta mañana",
+    "cuando yo estaba en el baño",
+    "entre las 7 y las 8 del mediodia"
+  ];
+
+  let rdm1 = Math.floor(Math.random() * sujeto.length);
+  let rdm2 = Math.floor(Math.random() * accion.length);
+  let rdm3 = Math.floor(Math.random() * objeto.length);
+  let rdm4 = Math.floor(Math.random() * tiempo.length);
+
+  document.querySelector("#excusa").innerHTML =
+    sujeto[rdm1] + accion[rdm2] + objeto[rdm3] + tiempo[rdm4];
 };
